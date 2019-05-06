81f8679413cfa91c37c41776409da93eb3e8ac67
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2019-05-05 17:51:59 -0700

    fixes typo

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
     'gatsby-plugin-react-helmet',
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
-    'gatsby-plugin-styled-components'
+    'gatsby-plugin-styled-components',
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
