4040e8cf6916e03ea489e7abd1386e72457f55d6
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2019-05-05 23:53:53 -0700

    adds normalize.css

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,12 +2,14 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Header from './header'
 import Footer from './footer'
+import { Normalize } from 'styled-normalize'
 
 const Layout = ({ children }) => {
   return (
     <div>
       <div>
         <Header />
+        <Normalize />
         {children}
       </div>
       <Footer />
